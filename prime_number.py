29e1462db0da1b6dab307207c835868939f20ea9
fatehhundal <fshundal@hotmail.com>
2025-05-03 12:11:47 +0800

    prime_number

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is a composite number.")
+            break
+        else:
+            print(f"{num} is a prime number.")
+else:
+    print(f"{num} is neither prime nor composite.")
